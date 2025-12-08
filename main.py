b906b949b4de732f3e2249408c53013f83f25544
Olivier Currie <olivier_currie@icloud.com>
2025-12-07 21:15:32 -0500

    comments

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from ui.main_window import App
 from db.database import init_db
 
 
-# need to encrypt/decrypt by chunks
+# need to add max file size
+# might need to create an about page
 # need to credit icons8 for the icons
+# need to add account info page
+# add input/output file history
 # add hover property for bound labels
 # style ui
-# more error handling
+# fix timestamp est
+
 
 if __name__ == "__main__":
     init_db()
